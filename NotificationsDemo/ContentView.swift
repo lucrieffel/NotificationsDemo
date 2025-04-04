57561c318628ce53d045d77a6cfd5c14dd261af1
Luc Rieffel <65513046+lucrieffel@users.noreply.github.com>
2025-04-04 16:18:53 -0700

    Initial Commit

---
+++ NotificationsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotificationsDemo
+//
+//  Created by Luc Rieffel on 4/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotificationsDemo/NotificationsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsDemoApp.swift
+//  NotificationsDemo
+//
+//  Created by Luc Rieffel on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
